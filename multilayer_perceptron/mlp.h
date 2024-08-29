1fb7fed2890f6462e3d40aca83d55829f0c6f23f
Dhruv Setty <dhruv.saideep@gmail.com>
2024-08-28 23:53:38 -0500

    Backpropagation implementation passes all written tests.

--- multilayer_perceptron/mlp.h
+++ multilayer_perceptron/mlp.h
@@ -8,6 +8,7 @@
 
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 #include "../common_definitions.h"
 
@@ -37,6 +38,10 @@ typedef struct NeuronNode {
 } Neuron;
 
 typedef struct MultilayerPerceptron {
+    int num_layers;
+    int num_weights;
+    int num_total_neurons;
+
     Neuron* first_layer;
     int num_first_layer_neurons;
 
@@ -58,7 +63,7 @@ float weighted_sum(Neuron neuron, int deriv_index, int deriving_variable) {
         result += neuron.prev_layer[i].output * neuron.prev_layer_weights[i];
     }
 
-    if (deriv_index >= 0) result -= neuron.bias;
+    if (deriv_index >= 0) result -= neuron.bias + 1;
 
     return result;
 }
@@ -71,6 +76,9 @@ MLP* build_mlp(int num_layers, int neurons_per_layer[],
                                 one_arg_activation_function function_per_layer[],  float weight_vals[]) {
 
     MLP* new_mlp = (MLP*) malloc(sizeof(MLP));
+    new_mlp->num_layers = num_layers;
+    new_mlp->num_weights = 0;
+    new_mlp->num_total_neurons = 0;
     Neuron* prev_layer = NULL;
     Neuron* current_layer = new_mlp->first_layer;
 
@@ -81,6 +89,8 @@ MLP* build_mlp(int num_layers, int neurons_per_layer[],
     for (int l = 0; l < num_layers; ++l) {
         current_layer = (Neuron*) malloc(sizeof(Neuron) * neurons_per_layer[l]);
         if (l == 0) new_mlp->first_layer = current_layer;
+
+        new_mlp->num_total_neurons += neurons_per_layer[l];
 
         for (int i = 0; i < neurons_per_layer[l]; ++i) {
             if (!prev_layer) {
@@ -93,6 +103,8 @@ MLP* build_mlp(int num_layers, int neurons_per_layer[],
                 current_layer[i].num_in_prev_layer = neurons_per_layer[l - 1];
 
                 int neurons_in_last_layer = neurons_per_layer[l - 1];
+
+                new_mlp->num_weights += neurons_per_layer[l - 1];
 
                 for (int j = 0; j < neurons_in_last_layer; ++j) {
                     if (weight_vals) current_layer[i].prev_layer_weights[j] = weight_vals[weights_added++];
@@ -109,7 +121,7 @@ MLP* build_mlp(int num_layers, int neurons_per_layer[],
             current_layer[i].next_layer = NULL;
             current_layer[i].num_in_next_layer = 0;
 
-            current_layer[i].output = 0; // neuron "switched off" when initialized
+            current_layer[i].output = NAN; // output set to NAN to indicate mlp hasn't computed anything yet
         }
 
         prev_layer = current_layer;
@@ -162,6 +174,14 @@ void compute_mlp_output(MLP* mlp, float inputs[]) {
 
 /* Emulates backward pass functionality of an MLP */
 void adjust_weights_and_biases(MLP* mlp, cost_function loss_function, float actual_outputs[]) {
+    // TODO: modify function to adjust weights and biases according to average of all training examples
+
+    float* stored_weights_adjustments = (float*) malloc(sizeof(float) * mlp->num_weights);
+    int swa_i = 0;
+
+    float* stored_bias_adjustments = (float*) malloc(sizeof(float) * mlp->num_total_neurons);
+    int sba_i = 0;
+
     float* last_layer_derivs = (float*) malloc(sizeof(float) * mlp->num_output_layer_neurons);
     int num_in_last_layer = mlp->num_output_layer_neurons;
     
@@ -174,21 +194,45 @@ void adjust_weights_and_biases(MLP* mlp, cost_function loss_function, float actu
 
         for (int i = 0; i < num_in_curr_layer; ++i) {
             for (int j = 0; j < num_in_last_layer; ++j) {
-                float jth_output_to_wsum_deriv = current_layer[i].next_layer[j].activation_function(weighted_sum(current_layer->next_layer[j], -1, 0), 1);
-
-                float jth_wsum_to_weight_ij_deriv = weighted_sum(current_layer[i].next_layer[j], i, WEIGHT);
-                current_layer[i].next_layer[j].prev_layer_weights[i] += jth_wsum_to_weight_ij_deriv;
-
-                float jth_wsum_to_bias_deriv = weighted_sum(current_layer[i].next_layer[j], i, BIAS);
-                current_layer[i].next_layer[j].bias += jth_wsum_to_bias_deriv;
-
+                float jth_output_to_wsum_deriv = current_layer[i].next_layer[j].activation_function(weighted_sum(current_layer[i].next_layer[j], -1, 0), 1);
                 float jth_wsum_to_ith_output_deriv = weighted_sum(current_layer[i].next_layer[j], i, OUTPUT);
                 cost_over_output_derivs[i] += jth_wsum_to_ith_output_deriv * jth_output_to_wsum_deriv * last_layer_derivs[j];
             }
         }
 
+        for (int j = 0; j < num_in_last_layer; ++j) {
+            for (int i = 0; i < num_in_curr_layer; ++i) {
+                float jth_output_to_wsum_deriv = current_layer[i].next_layer[j].activation_function(weighted_sum(current_layer[i].next_layer[j], -1, 0), 1);
+
+                float jth_wsum_to_weight_ij_deriv = weighted_sum(current_layer[i].next_layer[j], i, WEIGHT);
+                stored_weights_adjustments[swa_i++] = last_layer_derivs[j] * jth_output_to_wsum_deriv * jth_wsum_to_weight_ij_deriv;
+
+                float jth_wsum_to_bias_deriv = weighted_sum(current_layer[i].next_layer[j], i, BIAS);
+                stored_bias_adjustments[sba_i++] = last_layer_derivs[j] * jth_output_to_wsum_deriv * jth_wsum_to_bias_deriv;
+            }
+        }
+
         free(last_layer_derivs);
         last_layer_derivs = cost_over_output_derivs;
+
+        num_in_last_layer = num_in_curr_layer;
+        num_in_curr_layer = current_layer->num_in_prev_layer;
+    }
+
+    swa_i = 0;
+    sba_i = 0;
+
+    for (Neuron* current_layer = mlp->output_layer; current_layer->prev_layer; current_layer = current_layer->prev_layer) {
+        int num_in_current_layer = current_layer->prev_layer->num_in_next_layer;
+        int num_in_prev_layer = current_layer->num_in_prev_layer;
+
+        for (int j = 0; j < num_in_current_layer; ++j) {
+            for (int i = 0; i < num_in_prev_layer; ++i) {
+                current_layer[j].prev_layer_weights[i] -= stored_weights_adjustments[swa_i++];
+            }
+
+            current_layer[j].bias -= stored_bias_adjustments[sba_i++];
+        }
     }
 }
 
--- multilayer_perceptron/mlp_tests.c
+++ multilayer_perceptron/mlp_tests.c
@@ -13,15 +13,15 @@
 #define TRUE 1
 #define FALSE 0
 
-void mlp_test(MLP* mlp, const char* test_name, float inputs[], float correct_outputs[], int num_outputs, FILE* output_file) {
-    fprintf(output_file, "%s Test:\n", test_name);
+void mlp_ff_test(MLP* mlp, const char* test_name, float inputs[], float correct_outputs[], int num_outputs, FILE* output_file) {
+    fprintf(output_file, "%s Feed Forward Test:\n", test_name);
     fprintf(output_file, "------------------------------\n");
 
     compute_mlp_output(mlp, inputs);
 
     for (int i = 0; i < num_outputs; ++i) {
         fprintf(output_file, "\t- MLP Output %d: %f\n", i + 1, mlp->outputs[i]);
-        fprintf(output_file, "\t- Actual Output %d: %f\n", i + 1, correct_outputs[i]);
+        fprintf(output_file, "\t- Correct Output %d: %f\n", i + 1, correct_outputs[i]);
 
         assert(round_to_place(mlp->outputs[i], 5) == round_to_place(correct_outputs[i], 5));
         fprintf(output_file, "\t- Output %d is correct\n", i + 1);
@@ -31,8 +31,61 @@ void mlp_test(MLP* mlp, const char* test_name, float inputs[], float correct_out
     fprintf(output_file, "\n");
 }
 
+void mlp_bp_test(MLP* mlp, const char* test_name, float inputs[], float ideal_outputs[], cost_function loss_function, int num_outputs, FILE* output_file) {
+    fprintf(output_file, "%s Backpropagation Test:\n", test_name);
+    fprintf(output_file, "------------------------------\n");
+
+    compute_mlp_output(mlp, inputs);
+
+    fprintf(output_file, "- First Computed Output:\n");
+    for (int j = 0; j < num_outputs; ++j) {
+        fprintf(output_file, "\t[%d: %f]\n", j, mlp->outputs[j]);
+    }
+
+    fprintf(output_file, "- Ideal Output:\n");
+    for (int j = 0; j < num_outputs; ++j) {
+        fprintf(output_file, "\t[%d: %f]\n", j, ideal_outputs[j]);
+    }
+
+    float error = loss_function(mlp->outputs, ideal_outputs, num_outputs, -1);
+    fprintf(output_file, "- Error: %f\n", error);
+
+    int i = 1;
+    float prev_error = error;
+
+    while (error > 0.005) {
+        fprintf(output_file, "------------------------------\n");
+        fprintf(output_file, "- Iteration %d:\n", i);
+
+        adjust_weights_and_biases(mlp, loss_function, ideal_outputs);
+        compute_mlp_output(mlp, inputs);
+
+        fprintf(output_file, "\t- Computed Outputs after Backpropagation:\n");
+        for (int j = 0; j < num_outputs; ++j) {
+            fprintf(output_file, "\t[%d: %f]\n", j, mlp->outputs[j]);
+        }
+
+        error = loss_function(mlp->outputs, ideal_outputs, num_outputs, -1);
+        fprintf(output_file, "\t- Previous Error: %f\n", prev_error);
+        fprintf(output_file, "\t- New Error: %f\n", error);
+
+        assert(error < prev_error);
+        fprintf(output_file, "\t- Error Reduced Successfully!\n");
+
+        fprintf(output_file, "------------------------------\n");
+
+        prev_error = error;
+
+        ++i;
+    } 
+
+    fprintf(output_file, "\n");
+}
+
 int main() {
     FILE* output_file = fopen("mlp_tests.txt", "w");
+
+    fprintf(output_file, "FEED FORWARD TESTS:\n\n");
 
     /* Forward Pass Tests */
     int num_layers = 3;
@@ -59,14 +112,27 @@ int main() {
 
     MLP* multiple_output_mlp = build_mlp(num_layers, multiple_output_config, multiple_output_function_config, multiple_output_weights);
 
-    mlp_test(single_output_mlp, "SingleOutputMLPOne", single_output_input_one, single_output_key_one, 1, output_file);
-    mlp_test(single_output_mlp, "SingleOutputMLPTwo", single_output_input_two, single_output_key_two, 1, output_file);
+    mlp_ff_test(single_output_mlp, "SingleOutputMLPOne", single_output_input_one, single_output_key_one, 1, output_file);
+    mlp_ff_test(single_output_mlp, "SingleOutputMLPTwo", single_output_input_two, single_output_key_two, 1, output_file);
 
-    mlp_test(multiple_output_mlp, "MultipleOutputMLPOne", multiple_output_input_one, multiple_output_key_one, 2, output_file);
-    mlp_test(multiple_output_mlp, "MultipleOutputMLPOne", multiple_output_input_two, multiple_output_key_two, 2, output_file);
+    mlp_ff_test(multiple_output_mlp, "MultipleOutputMLPOne", multiple_output_input_one, multiple_output_key_one, 2, output_file);
+    mlp_ff_test(multiple_output_mlp, "MultipleOutputMLPOne", multiple_output_input_two, multiple_output_key_two, 2, output_file);
 
     /* Backward pass tests */
+    fprintf(output_file, "\nBACKWARD PROPAGATION TESTS:\n\n");
+
     cost_function backprop_test_cost_func = mse;
+
+    float single_output_ideal_output_one[1] = {1.0};
+    float single_output_ideal_output_two[1] = {0.0};
+
+    float multiple_output_ideal_outputs[2] = {1.0, 0.0};
+
+    mlp_bp_test(single_output_mlp, "SingleOutputMLP", single_output_input_one, single_output_ideal_output_one, backprop_test_cost_func,  1, output_file);
+    mlp_bp_test(single_output_mlp, "SingleOutputMLP", single_output_input_one, single_output_ideal_output_two, backprop_test_cost_func,  1, output_file); // testing how model learns with decently large error
+    mlp_bp_test(single_output_mlp, "SingleOutputMLP", single_output_input_one, single_output_ideal_output_two, backprop_test_cost_func,  1, output_file); // testing how model learns with very small error
+
+    mlp_bp_test(multiple_output_mlp, "MultipleOutputMLP", multiple_output_input_one, multiple_output_ideal_outputs, backprop_test_cost_func, 2, output_file);
 
     fclose(output_file);
 }
