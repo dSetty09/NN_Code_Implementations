618cdb8d0dc6f3214a1b8e47da6ae0f097e6aa6f
Dhruv Setty <dhruv.saideep@gmail.com>
2025-01-03 00:52:14 -0600

    ann file

---
+++ include/ann/ann.h
@@ -0,0 +1,44 @@
+#ifndef ANN_H
+#define ANN_H
+
+#include "dense_layer.h"
+
+#ifdef __cplusplus 
+extern "C" {
+#endif
+
+/* GLOBAL CONSTANTS */
+
+static const unsigned int MSE = 0;
+static const unsigned int CROSS_ENTROPY = 1; 
+
+
+/* DEFINED STRUCTS */
+
+typedef struct ann {
+    DenseLayer* layers; // the computing layers (i.e. hidden layers + output layer)
+    int num_layers; // the number of computing layers
+
+    float* inputs; // the "input" layer consisting of inputs
+    int num_inputs; // the number of inputs
+
+    int batch_size; // size of batch while learning via gradient descent
+    int cost_func; // the cost function
+} ArtificialNeuralNetwork;
+
+/*
+ * Enables an artificial neural network to learn from a given set or subset of training data.
+ *
+ * @param ann | The artificial neural network being trained
+ * @param X_train | The inputs data for training the model
+ * @param y_train | The output data for training the model 
+ */
+void learn(ArtificialNeuralNetwork* ann, float** X_train, float* y_train) {
+
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
